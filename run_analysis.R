69841e36178b2d6505d6274fb14a2a7de3874a23
Daniel Bancovsky <dani_b@terra.com.br>
2015-06-12 01:27:52 +0300

    missing only the extraction and step 5

--- run_analysis.R
+++ run_analysis.R
@@ -3,8 +3,7 @@
 #open the Features and activity_labels data
 
 features<-read.table("Courseproject/features.txt", col.names=c("vector", "formula"))
-actlabels<-read.table("Courseproject/activity_labels.txt", row.names=c("1","2","3","4","5","6"), col.names=c("label", "activity"))
-activities<-matrix(actlabels[,"activity"])
+activitylabels<-read.table("Courseproject/activity_labels.txt", row.names=c("1","2","3","4","5","6"), col.names=c("label", "activity"))
 #select the Formulas used in Features and the Activity used in activity_labels
 #to name the test/train columns and better see the data instead of numbers
 
@@ -14,15 +13,42 @@ formula<-features[,"formula"]
 #open test files
 Xtest<-read.table("Courseproject/test/X_test.txt", col.names=formula)
 Ytest<-read.table("Courseproject/test/Y_test.txt")
-Subtest<-read.table("Courseproject/test/subject_test.txt")
+Subtest<-read.table("Courseproject/test/subject_test.txt", col.names="subject")
 #open train files
 Xtrain<-read.table("Courseproject/train/X_train.txt", col.names=formula)
 Ytrain<-read.table("Courseproject/train/Y_train.txt")
-Subtrain<-read.table("Courseproject/train/subject_train.txt")
+Subtrain<-read.table("Courseproject/train/subject_train.txt", col.names="subject")
 
 #join tables
 Xtotal<-rbind(Xtest, Xtrain)
 Ytotal<-rbind(Ytest, Ytrain)
 Subtotal<-rbind(Subtest, Subtrain)
 
-if(Ytest[i,1])
+#parse the activity label with its code
+activities<-data.frame()
+for(i in 1:nrow(Ytotal)){
+        lista<-activitylabels[activitylabels$label==Ytotal[i,],]
+        activities<-rbind(activities,lista)
+        
+}
+
+#join all tables in one
+onetable<-cbind(Xtotal,activities,Subtotal)
+
+#find variables with mean ()
+#IMPORTANT: library(stringr)
+library(stringr)
+vartext<-paste(formula)
+split<-strsplit(vartext,"-")#separate words of variable names
+
+#gets the numbers of the variables with the word "mean()"
+goodvars<-data.frame()
+for (i in 1:length(split)){ 
+        if(any(split[[i]]=="mean()")){
+                good<-data.frame(i)
+                goodvars<-rbind(goodvars,good)
+        }
+        
+}
+#extract only the variables with mean()
+extractedtable<-onetable[,c(goodvars$i,onetable$activity,onetable$subject)]
